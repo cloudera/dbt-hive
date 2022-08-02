7ad4ec9061186095a17b129c726b06bc89254891
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-08-02 12:32:16 +0530

    author update in setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import find_namespace_packages, setup
 package_name = "dbt-hive"
 # make sure this always matches dbt/adapters/hive/__version__.py
 package_version = "1.1.0b3"
-description = """The experient hive adapter plugin for dbt"""
+description = """The Hive adapter plugin for dbt"""
 
 setup(
     name=package_name,
     version=package_version,
     description=description,
     long_description=description,
-    author='Nguyen Huu Bach',
-    author_email='bachng@gmail.com',
-    url='https://github.com/bachng2017/dbt-hive',
+    author="Cloudera",
+    author_email="innovation-feedback@cloudera.com",
+    url="https://github.com/cloudera/dbt-hive",
     packages=find_namespace_packages(include=['dbt', 'dbt.*']),
     package_data={
         "dbt": [
