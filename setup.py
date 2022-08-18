2720dd0f10dedf1f4673ad65d9639e210350688e
V. Ganesh <tovganesh@yahoo.co.in>
2022-08-18 17:09:16 +0530

    update setup.py so that the correct version of dbt-core is picked up (#7)

--- setup.py
+++ setup.py
@@ -1,16 +1,48 @@
 #!/usr/bin/env python
+import os
+import re
 from setuptools import find_namespace_packages, setup
+
+# pull long description from README
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), 'r', encoding='utf8') as f:
+    long_description = f.read()
+
+# get this package's version from dbt/adapters/<name>/__version__.py
+def _get_plugin_version_dict():
+    _version_path = os.path.join(
+        this_directory, 'dbt', 'adapters', 'hive', '__version__.py'
+    )
+    _semver = r'''(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)'''
+    _pre = r'''((?P<prekind>a|b|rc)(?P<pre>\d+))?'''
+    _version_pattern = fr'''version\s*=\s*["']{_semver}{_pre}["']'''
+    with open(_version_path) as f:
+        match = re.search(_version_pattern, f.read().strip())
+        if match is None:
+            raise ValueError(f'invalid version at {_version_path}')
+        return match.groupdict()
+
+
+# require a compatible minor version (~=), prerelease if this is a prerelease
+def _get_dbt_core_version():
+    parts = _get_plugin_version_dict()
+    minor = "{major}.{minor}.0".format(**parts)
+    pre = (parts["prekind"]+"1" if parts["prekind"] else "")
+    return f"{minor}{pre}"
 
 package_name = "dbt-hive"
 # make sure this always matches dbt/adapters/hive/__version__.py
 package_version = "1.1.0b3"
 description = """The Hive adapter plugin for dbt"""
 
+dbt_core_version = _get_dbt_core_version()
+
 setup(
     name=package_name,
     version=package_version,
     description=description,
-    long_description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Cloudera",
     author_email="innovation-feedback@cloudera.com",
     url="https://github.com/cloudera/dbt-hive",
@@ -25,7 +57,7 @@ setup(
         ]
     },
     install_requires=[
-        "dbt-core==1.1.0",
+        'dbt-core~={}'.format(dbt_core_version),
         "setuptools>=40.3.0",
         "impyla>=0.18a5"
     ]
