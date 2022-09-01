5f671f98a3d73d04c4eda9dea3f8e2a33fe45bbd
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-09-01 11:56:44 +0530

    Fix an issue with incremental merge where the merge SQL was not correctly generated. (#25)
    Testplan:
    1) Clone example repo: https://github.com/cloudera/dbt-hive-example
    2) Modify models/marts/covid/covid_cases.sql to include the following config:
    {{
        config(
            materialized='incremental',
            incremental_strategy='merge',
            unique_key='country',
            merge_update_columns=['cases', 'deaths']
        )
    }}
    3) Do dbt run --full-refresh
    4) Then change the source table raw_cases to include more data and update some rows
    5) Do dbt run - this should succeed with new data reflected in the incremental model

--- dbt/include/hive/macros/materializations/incremental/validate.sql
+++ dbt/include/hive/macros/materializations/incremental/validate.sql
@@ -39,17 +39,6 @@
     Invalid incremental strategy provided: {{ raw_strategy }}
     Expected one of: 'append', 'merge', 'insert_overwrite'
   {%- endset %}
-
-  {% set invalid_merge_msg -%}
-    Invalid incremental strategy provided: {{ raw_strategy }}
-    You can only choose this strategy when file_format is set to 'delta'
-  {%- endset %}
-  
-  {% set invalid_insert_overwrite_delta_msg -%}
-    Invalid incremental strategy provided: {{ raw_strategy }}
-    You cannot use this strategy when file_format is set to 'delta'
-    Use the 'append' or 'merge' strategy instead
-  {%- endset %}
   
   {% set invalid_insert_overwrite_endpoint_msg -%}
     Invalid incremental strategy provided: {{ raw_strategy }}
@@ -60,12 +49,6 @@
   {% if raw_strategy not in ['append', 'merge', 'insert_overwrite'] %}
     {% do exceptions.raise_compiler_error(invalid_strategy_msg) %}
   {%-else %}
-    {% if raw_strategy == 'merge' and file_format != 'delta' %}
-      {% do exceptions.raise_compiler_error(invalid_merge_msg) %}
-    {% endif %}
-    {% if raw_strategy == 'insert_overwrite' and file_format == 'delta' %}
-      {% do exceptions.raise_compiler_error(invalid_insert_overwrite_delta_msg) %}
-    {% endif %}
     {% if raw_strategy == 'insert_overwrite' and target.endpoint %}
       {% do exceptions.raise_compiler_error(invalid_insert_overwrite_endpoint_msg) %}
     {% endif %}
