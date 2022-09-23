97df5e79c7488aaaf60c9e709858b775c006e451
Tapas Kumar Senapati <tksenapa@ncsu.edu>
2022-09-23 13:03:39 +0530

    Added cml instrumentation

--- dbt/adapters/hive/connections.py
+++ dbt/adapters/hive/connections.py
@@ -94,6 +94,8 @@ class HiveCredentials(Credentials):
         tracker.usage_tracking = self.usage_tracking
         # get platform information for tracking
         tracker.populate_platform_info(self, ver)
+        # get cml information for tracking
+        tracker.populate_cml_info()
         # generate unique ids for tracking
         tracker.populate_unique_ids(self)
 
