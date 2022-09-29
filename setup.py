5d5d6529f0ed66e19d1357e0e2b999f76e594790
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-09-29 11:21:53 +0530

    update impyla to stable version (#48)

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     install_requires=[
         'dbt-core~={}'.format(dbt_core_version),
         "setuptools>=40.3.0",
-        "impyla>=0.18a5",
+        "impyla==0.18",
         "sqlparams>=3.0.0",
         "python-decouple>=3.6"
     ]
