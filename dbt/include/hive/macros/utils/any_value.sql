425e3f15e15f2daf60515abba81f97687ee9e028
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-10-19 22:06:37 +0530

    any_value tweak

--- dbt/include/hive/macros/utils/any_value.sql
+++ dbt/include/hive/macros/utils/any_value.sql
@@ -1,4 +1,4 @@
 {% macro hive__any_value(expression) -%}
     {#-- return any value (non-deterministic)  --#}
-    first_value({{ expression }}) over(partition by {{expression}} order by {{expression}})
+    first_value({{ expression }}) over()
 {%- endmacro %}
