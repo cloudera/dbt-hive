bc83720c87f08777da482518530fda5a693900f6
Sujit Kumar Pulapura <spulapura@cloudera.com>
2022-10-21 12:32:10 +0530

    Update to support TestRight

--- dbt/include/hive/macros/utils/right.sql
+++ dbt/include/hive/macros/utils/right.sql
@@ -1,9 +1,13 @@
 {% macro hive__right(string_text, length_expression) %}
-    
-    substr(
+
+    case when {{ length_expression }} = 0
+        then NULL
+    else
+        substr(
         {{ string_text }},
         (length({{ string_text }})-cast({{ length_expression }} as int)+1),
         length({{ string_text }})-1
     )
+    end
     
 {%- endmacro -%}
