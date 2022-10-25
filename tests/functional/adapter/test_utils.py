dd9fbd48fe77a7a4d76a31421e4efb65e8dd36b2
Sujit Kumar Pulapura <spulapura@cloudera.com>
2022-10-25 17:57:51 +0530

    test tuils changes for addtional macro tests

--- tests/functional/adapter/test_utils.py
+++ tests/functional/adapter/test_utils.py
@@ -14,15 +14,36 @@
 import pytest
 from dbt.tests.adapter.utils.base_utils import BaseUtils
 from dbt.tests.adapter.utils.test_any_value import BaseAnyValue
+from dbt.tests.adapter.utils.test_bool_or import BaseBoolOr
+from dbt.tests.adapter.utils.test_cast_bool_to_text import BaseCastBoolToText
 from dbt.tests.adapter.utils.test_concat import BaseConcat
 from dbt.tests.adapter.utils.test_escape_single_quotes import BaseEscapeSingleQuotesQuote
 from dbt.tests.adapter.utils.test_escape_single_quotes import BaseEscapeSingleQuotesBackslash
 from dbt.tests.adapter.utils.test_position import BasePosition
 from dbt.tests.adapter.utils.test_replace import BaseReplace
 from dbt.tests.adapter.utils.test_right import BaseRight
+from dbt.tests.adapter.utils.test_safe_cast import BaseSafeCast
 from dbt.tests.adapter.utils.test_split_part import BaseSplitPart
 from dbt.tests.adapter.utils.test_string_literal import BaseStringLiteral
 
+from dbt.tests.adapter.utils.fixture_any_value import (
+    seeds__data_any_value_csv,
+    seeds__data_any_value_expected_csv,
+    models__test_any_value_sql,
+    models__test_any_value_yml,
+)
+
+from dbt.tests.adapter.utils.fixture_bool_or import (
+    seeds__data_bool_or_csv,
+    seeds__data_bool_or_expected_csv,
+    models__test_bool_or_sql,
+    models__test_bool_or_yml,
+)
+
+from dbt.tests.adapter.utils.fixture_cast_bool_to_text import (
+    models__test_cast_bool_to_text_sql,
+    models__test_cast_bool_to_text_yml,
+)
 
 from dbt.tests.adapter.utils.fixture_concat import (
     seeds__data_concat_csv,
@@ -54,6 +75,12 @@ from dbt.tests.adapter.utils.fixture_right import (
     models__test_right_yml,
 )
 
+from dbt.tests.adapter.utils.fixture_safe_cast import (
+    seeds__data_safe_cast_csv,
+    models__test_safe_cast_sql,
+    models__test_safe_cast_yml,
+)
+
 from dbt.tests.adapter.utils.fixture_split_part import (
     seeds__data_split_part_csv,
     models__test_split_part_sql,
@@ -65,14 +92,141 @@ from dbt.tests.adapter.utils.fixture_escape_single_quotes import (
      models__test_escape_single_quotes_yml,
 )
 
+models__test_any_value_sql = """
+with util_data as (
+
+    select * from {{ ref('data_any_value') }}
+
+),
+
+data_output as (
+
+    select * from {{ ref('data_any_value_expected') }}
+
+),
+
+calculate as (
+    select
+        key_name,
+        {{ any_value('static_col') }} as static_col,
+        count(id) as num_rows
+    from util_data
+    group by key_name
+)
+
+select
+    calculate.num_rows as actual,
+    data_output.num_rows as expected
+from calculate
+left join data_output
+on calculate.key_name = data_output.key_name
+and calculate.static_col = data_output.static_col
+"""
+
+class TestAnyValue(BaseAnyValue):
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {
+            "data_any_value.csv": seeds__data_any_value_csv,
+            "data_any_value_expected.csv": seeds__data_any_value_expected_csv,
+        }
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "test_any_value.yml": models__test_any_value_yml,
+            "test_any_value.sql": self.interpolate_macro_namespace(
+                models__test_any_value_sql, "any_value"
+            ),
+        }
+
+models__test_bool_or_sql = """
+with util_data as (
+
+    select * from {{ ref('data_bool_or') }}
+
+),
+
+data_output as (
+
+    select * from {{ ref('data_bool_or_expected') }}
+
+),
+
+calculate as (
+
+    select
+        key,
+        {{ bool_or('val1 = val2') }} as value
+    from util_data
+    group by key
+
+)
+
+select
+    calculate.value as actual,
+    data_output.value as expected
+from calculate
+left join data_output
+on calculate.key = data_output.key
+"""
+
+class TestBoolOr(BaseBoolOr):
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {
+            "data_bool_or.csv": seeds__data_bool_or_csv,
+            "data_bool_or_expected.csv": seeds__data_bool_or_expected_csv,
+        }
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "test_bool_or.yml": models__test_bool_or_yml,
+            "test_bool_or.sql": self.interpolate_macro_namespace(
+                models__test_bool_or_sql, "bool_or"
+            ),
+        }
+
+models__test_cast_bool_to_text_sql = """
+with util_data as (
+
+    select 0=1 as input, 'false' as expected union all
+    select 1=1 as input, 'true' as expected union all
+    select null as input, null as expected
+
+)
+
+select
+
+    {{ cast_bool_to_text("input") }} as actual,
+    expected
+
+from util_data
+"""
+
+class TestCastBoolToText(BaseCastBoolToText):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "test_cast_bool_to_text.yml": models__test_cast_bool_to_text_yml,
+            "test_cast_bool_to_text.sql": self.interpolate_macro_namespace(
+                models__test_cast_bool_to_text_sql, "cast_bool_to_text"
+            ),
+        }
+
 
 models__test_concat_sql = """
 with util_data as (
+
     select * from {{ ref('data_concat') }}
+
 )
+
 select
     {{ concat(['input_1', 'input_2']) }} as actual,
     output as expected
+
 from util_data
 """
 class TestConcat(BaseConcat):
@@ -175,6 +329,34 @@ class TestRight(BaseRight):
             "test_right.sql": self.interpolate_macro_namespace(models__test_right_sql, "right"),
         }
 
+
+models__test_safe_cast_sql = """
+with util_data as (
+
+    select * from {{ ref('data_safe_cast') }}
+
+)
+
+select
+    {{ safe_cast('field', api.Column.translate_type('string')) }} as actual,
+    output as expected
+
+from util_data
+"""
+class TestSafeCast(BaseSafeCast):
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {"data_safe_cast.csv": seeds__data_safe_cast_csv}
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "test_safe_cast.yml": models__test_safe_cast_yml,
+            "test_safe_cast.sql": self.interpolate_macro_namespace(
+                self.interpolate_macro_namespace(models__test_safe_cast_sql, "safe_cast"),
+                "type_string",
+            ),
+        }
 
 models__test_split_part_sql = """
 with util_data as (
