506e78e7b53729a814fd57851cb327e8b0417826
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-10-26 21:18:52 +0530

    fix datediff

--- dbt/include/hive/macros/utils/datediff.sql
+++ dbt/include/hive/macros/utils/datediff.sql
@@ -1,70 +1,69 @@
-
 {% macro assert_not_null(function, arg) %}
-    coalesce(cast({{function}}({{arg}}) as bigint), nvl2({{function}}({{arg}}), istrue({{function}}({{arg}}) is not null), null))
-{% endmacro %}
-
-{% macro assert_not_null2(function, arg1, arg2) %}
-    coalesce(cast({{function}}({{arg1}}, {{arg2}}) as bigint), nvl2({{function}}({{arg1}}, {{arg2}}), istrue({{function}}({{arg1}}, {{arg2}}) is not null), null))
+    coalesce({{function}}({{arg}}), nvl({{function}}({{arg}}), assert_true({{function}}({{arg}}) is not null), null))
 {% endmacro %}
 
 {% macro hive__datediff(first_date, second_date, datepart) %}
 
-    {# make sure the dates are real, otherwise raise an error asap #}
-    {% set first_date = assert_not_null2('date_part', '"EPOCH"', first_date) %}
-    {% set second_date = assert_not_null2('date_part', '"EPOCH"', second_date) %}
+    {%- if datepart in ['day', 'week', 'month', 'quarter', 'year'] -%}
+
+        {# make sure the dates are real, otherwise raise an error asap #}
+        {% set first_date = assert_not_null('date', first_date) %}
+        {% set second_date = assert_not_null('date', second_date) %}
+
+    {%- endif -%}
 
     {%- if datepart == 'day' -%}
 
-        datediff(cast({{second_date}} as timestamp), cast({{first_date}} as timestamp))
+        datediff({{second_date}}, {{first_date}})
 
     {%- elif datepart == 'week' -%}
 
         case when {{first_date}} < {{second_date}}
-            then floor(datediff(cast({{second_date}} as timestamp), cast({{first_date}} as timestamp))/7)
-            else ceil(datediff(cast({{second_date}} as timestamp), cast({{first_date}} as timestamp))/7)
+            then floor(datediff({{second_date}}, {{first_date}})/7)
+            else ceil(datediff({{second_date}}, {{first_date}})/7)
             end
 
         -- did we cross a week boundary (Sunday)?
         + case
-            when {{first_date}} < {{second_date}} and dayofweek(cast({{second_date}} as timestamp)) < dayofweek(cast({{first_date}} as timestamp)) then 1
-            when {{first_date}} > {{second_date}} and dayofweek(cast({{second_date}} as timestamp)) > dayofweek(cast({{first_date}} as timestamp)) then -1
+            when {{first_date}} < {{second_date}} and dayofweek({{second_date}}) < dayofweek({{first_date}}) then 1
+            when {{first_date}} > {{second_date}} and dayofweek({{second_date}}) > dayofweek({{first_date}}) then -1
             else 0 end
 
     {%- elif datepart == 'month' -%}
 
         case when {{first_date}} < {{second_date}}
-            then floor(months_between(cast({{second_date}} as timestamp), cast({{first_date}} as timestamp)))
-            else ceil(months_between(cast({{second_date}} as timestamp), cast({{first_date}} as timestamp)))
+            then floor(months_between(date({{second_date}}), date({{first_date}})))
+            else ceil(months_between(date({{second_date}}), date({{first_date}})))
             end
 
         -- did we cross a month boundary?
         + case
-            when {{first_date}} < {{second_date}} and dayofmonth(cast({{second_date}} as timestamp)) < dayofmonth(cast({{first_date}} as timestamp)) then 1
-            when {{first_date}} > {{second_date}} and dayofmonth(cast({{second_date}} as timestamp)) > dayofmonth(cast({{first_date}} as timestamp)) then -1
+            when {{first_date}} < {{second_date}} and dayofmonth({{second_date}}) < dayofmonth({{first_date}}) then 1
+            when {{first_date}} > {{second_date}} and dayofmonth({{second_date}}) > dayofmonth({{first_date}}) then -1
             else 0 end
 
     {%- elif datepart == 'quarter' -%}
 
         case when {{first_date}} < {{second_date}}
-            then floor(months_between(cast({{second_date}} as timestamp), cast({{first_date}} as timestamp))/3)
-            else ceil(months_between(cast({{second_date}} as timestamp), cast({{first_date}} as timestamp))/3)
+            then floor(months_between(date({{second_date}}), date({{first_date}}))/3)
+            else ceil(months_between(date({{second_date}}), date({{first_date}}))/3)
             end
 
         -- did we cross a quarter boundary?
         + case
             when {{first_date}} < {{second_date}} and (
-                (dayofyear(cast({{second_date}} as timestamp)) - (quarter(cast({{second_date}} as timestamp)) * 365/4))
-                < (dayofyear(cast({{first_date}} as timestamp)) - (quarter(cast({{first_date}} as timestamp)) * 365/4))
+                (weekofyear({{second_date}})*7 - (quarter({{second_date}}) * 365/4))
+                < (weekofyear({{first_date}})*7 - (quarter({{first_date}}) * 365/4))
             ) then 1
             when {{first_date}} > {{second_date}} and (
-                (dayofyear(cast({{second_date}} as timestamp)) - (quarter(cast({{second_date}} as timestamp)) * 365/4))
-                > (dayofyear(cast({{first_date}} as timestamp)) - (quarter(cast({{first_date}} as timestamp)) * 365/4))
+                (weekofyear({{second_date}})*7 - (quarter({{second_date}}) * 365/4))
+                > (weekofyear({{first_date}})*7 - (quarter({{first_date}}) * 365/4))
             ) then -1
             else 0 end
 
     {%- elif datepart == 'year' -%}
 
-        year(cast({{second_date}} as timestamp)) - year(cast({{first_date}} as timestamp))
+        year({{second_date}}) - year({{first_date}})
 
     {%- elif datepart in ('hour', 'minute', 'second', 'millisecond', 'microsecond') -%}
 
@@ -79,16 +78,19 @@
 
         case when {{first_date}} < {{second_date}}
             then ceil((
-                {{second_date}} - {{first_date}}
+                {# make sure the timestamps are real, otherwise raise an error asap #}
+                {{ assert_not_null('unix_timestamp', second_date) }}
+                - {{ assert_not_null('unix_timestamp', first_date) }}
             ) / {{divisor}})
             else floor((
-                {{second_date}} - {{first_date}}
+                {{ assert_not_null('unix_timestamp', second_date) }}
+                - {{ assert_not_null('unix_timestamp', first_date) }}
             ) / {{divisor}})
             end
 
             {% if datepart == 'millisecond' %}
-                + cast(date_part("MILLISECOND", cast({{second_date}} as timestamp)) as int)
-                - cast(date_part("MILLISECOND", cast({{first_date}} as timestamp)) as int)
+                + cast(date_format({{second_date}}, 'SSS') as int)
+                - cast(date_format({{first_date}}, 'SSS') as int)
             {% endif %}
 
             {% if datepart == 'microsecond' %}
@@ -96,8 +98,8 @@
                 -- Spark doesn't really support microseconds, so this is a massive hack!
                 -- It will only work if the timestamp-string is of the format
                 -- 'yyyy-MM-dd-HH mm.ss.SSSSSS'
-                + cast(regexp_extract(cast(cast({{second_date}} as timestamp) as string), '{{capture_str}}', 1) as int)
-                - cast(regexp_extract(cast(cast({{first_date}} as timestamp) as string), '{{capture_str}}', 1) as int)
+                + cast(regexp_extract({{second_date}}, '{{capture_str}}', 1) as int)
+                - cast(regexp_extract({{first_date}}, '{{capture_str}}', 1) as int)
             {% endif %}
 
     {%- else -%}
