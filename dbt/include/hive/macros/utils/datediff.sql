84aadd2f2ff25fc6f3dac14395ce02582223304a
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-11-03 21:18:56 +0530

    restructure datediff

--- dbt/include/hive/macros/utils/datediff.sql
+++ dbt/include/hive/macros/utils/datediff.sql
@@ -1,16 +1,4 @@
-{% macro assert_not_null(function, arg) %}
-    coalesce({{function}}({{arg}}), nvl({{function}}({{arg}}), assert_true({{function}}({{arg}}) is not null), null))
-{% endmacro %}
-
 {% macro hive__datediff(first_date, second_date, datepart) %}
-
-    {%- if datepart in ['day', 'week', 'month', 'quarter', 'year'] -%}
-
-        {# make sure the dates are real, otherwise raise an error asap #}
-        {% set first_date = assert_not_null('date', first_date) %}
-        {% set second_date = assert_not_null('date', second_date) %}
-
-    {%- endif -%}
 
     {%- if datepart == 'day' -%}
 
@@ -18,48 +6,15 @@
 
     {%- elif datepart == 'week' -%}
 
-        case when {{first_date}} < {{second_date}}
-            then floor(datediff({{second_date}}, {{first_date}})/7)
-            else ceil(datediff({{second_date}}, {{first_date}})/7)
-            end
-
-        -- did we cross a week boundary (Sunday)?
-        + case
-            when {{first_date}} < {{second_date}} and dayofweek({{second_date}}) < dayofweek({{first_date}}) then 1
-            when {{first_date}} > {{second_date}} and dayofweek({{second_date}}) > dayofweek({{first_date}}) then -1
-            else 0 end
+        weekofyear({{second_date}}) -  weekofyear({{first_date}})
 
     {%- elif datepart == 'month' -%}
 
-        case when {{first_date}} < {{second_date}}
-            then floor(months_between(date({{second_date}}), date({{first_date}})))
-            else ceil(months_between(date({{second_date}}), date({{first_date}})))
-            end
-
-        -- did we cross a month boundary?
-        + case
-            when {{first_date}} < {{second_date}} and dayofmonth({{second_date}}) < dayofmonth({{first_date}}) then 1
-            when {{first_date}} > {{second_date}} and dayofmonth({{second_date}}) > dayofmonth({{first_date}}) then -1
-            else 0 end
+        months_between({{second_date}}, {{first_date}})
 
     {%- elif datepart == 'quarter' -%}
 
-        case when {{first_date}} < {{second_date}}
-            then floor(months_between(date({{second_date}}), date({{first_date}}))/3)
-            else ceil(months_between(date({{second_date}}), date({{first_date}}))/3)
-            end
-
-        -- did we cross a quarter boundary?
-        + case
-            when {{first_date}} < {{second_date}} and (
-                (weekofyear({{second_date}})*7 - (quarter({{second_date}}) * 365/4))
-                < (weekofyear({{first_date}})*7 - (quarter({{first_date}}) * 365/4))
-            ) then 1
-            when {{first_date}} > {{second_date}} and (
-                (weekofyear({{second_date}})*7 - (quarter({{second_date}}) * 365/4))
-                > (weekofyear({{first_date}})*7 - (quarter({{first_date}}) * 365/4))
-            ) then -1
-            else 0 end
+        months_between({{second_date}}, {{first_date}}) / 3
 
     {%- elif datepart == 'year' -%}
 
@@ -76,13 +31,10 @@
 
         case when {{first_date}} < {{second_date}}
             then ceil((
-                {# make sure the timestamps are real, otherwise raise an error asap #}
-                {{ assert_not_null('unix_timestamp', second_date) }}
-                - {{ assert_not_null('unix_timestamp', first_date) }}
+                unix_timestamp( {{second_date}} ) - unix_timestamp( {{first_date}} ) 
             ) / {{divisor}})
             else floor((
-                {{ assert_not_null('unix_timestamp', second_date) }}
-                - {{ assert_not_null('unix_timestamp', first_date) }}
+                unix_timestamp( {{second_date}} ) - unix_timestamp( {{first_date}} ) 
             ) / {{divisor}})
             end
 
