738164e1cf67c3b39d24a95040e5c8d8fb8fb18b
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-11-21 19:41:55 +0530

    add basic docs tests (#66)
    * add basic docs tests
    
    * fix an issue with parsing metadata to generate docs
    
    * change tests to reflect hive catalog
    
    * change username to user for test profile
    
    * move out doc tests to new file, cleanup test_basics
    
    * fix docs tests for hive

--- tests/functional/adapter/test_basic.py
+++ tests/functional/adapter/test_basic.py
@@ -1,18 +1,13 @@
 import pytest
 
-from dbt.tests.util import run_dbt, check_relations_equal
-from dbt.tests.util import run_dbt, check_result_nodes_by_name
-
 from dbt.tests.adapter.basic.test_base import BaseSimpleMaterializations
 from dbt.tests.adapter.basic.test_singular_tests import BaseSingularTests
-from dbt.tests.adapter.basic.test_singular_tests_ephemeral import BaseSingularTestsEphemeral
 from dbt.tests.adapter.basic.test_empty import BaseEmpty
 from dbt.tests.adapter.basic.test_ephemeral import BaseEphemeral
 from dbt.tests.adapter.basic.test_incremental import BaseIncremental
 from dbt.tests.adapter.basic.test_generic_tests import BaseGenericTests
-from dbt.tests.adapter.basic.test_snapshot_check_cols import BaseSnapshotCheckCols
-from dbt.tests.adapter.basic.test_snapshot_timestamp import BaseSnapshotTimestamp
 from dbt.tests.adapter.basic.test_adapter_methods import BaseAdapterMethod
+from dbt.tests.adapter.utils.base_utils import BaseUtils
 
 class TestSimpleMaterializationsHive(BaseSimpleMaterializations):
    pass
@@ -22,35 +17,21 @@ class TestSingularTestsHive(BaseSingularTests):
    pass
 
 
-# NG
-# ephemeral with CTE does not work with hive
-# class TestSingularTestsEphemeralHive(BaseSingularTestsEphemeral):
-#   pass
-
 class TestEmptyHive(BaseEmpty):
    pass
 
-
 class TestEphemeralHive(BaseEphemeral):
    pass
-# 
-# 
+
 class TestIncrementalHive(BaseIncremental):
    pass
-# 
-# 
+
+
 class TestGenericTestsHive(BaseGenericTests):
    pass
-# 
-# NG 
-# class TestSnapshotCheckColsHive(BaseSnapshotCheckCols):
-#    pass
-# 
-# NG
-# class TestSnapshotTimestampHive(BaseSnapshotTimestamp):
-#     pass
-# 
-#
 
 class TestBaseAdapterMethod(BaseAdapterMethod):
     pass
+        
+class TestBaseUtilsHive(BaseUtils):
+     pass
