6ddedf151fe98de90786bce663cea84d31a4f3d8
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-11-22 10:54:32 +0530

    add new test TestBaseIncrementalNotSchemaChange (#74)

--- tests/functional/adapter/test_basic.py
+++ tests/functional/adapter/test_basic.py
@@ -4,7 +4,7 @@ from dbt.tests.adapter.basic.test_base import BaseSimpleMaterializations
 from dbt.tests.adapter.basic.test_singular_tests import BaseSingularTests
 from dbt.tests.adapter.basic.test_empty import BaseEmpty
 from dbt.tests.adapter.basic.test_ephemeral import BaseEphemeral
-from dbt.tests.adapter.basic.test_incremental import BaseIncremental
+from dbt.tests.adapter.basic.test_incremental import BaseIncremental, BaseIncrementalNotSchemaChange
 from dbt.tests.adapter.basic.test_generic_tests import BaseGenericTests
 from dbt.tests.adapter.basic.test_adapter_methods import BaseAdapterMethod
 from dbt.tests.adapter.utils.base_utils import BaseUtils
@@ -35,3 +35,19 @@ class TestBaseAdapterMethod(BaseAdapterMethod):
         
 class TestBaseUtilsHive(BaseUtils):
      pass
+
+incremental_not_schema_change_sql = """
+{{ config(materialized="incremental", incremental_strategy="append") }}
+select
+    concat(concat('1', '-'), cast(current_timestamp() as string)) as user_id_current_time,
+    {% if is_incremental() %}
+        'thisis18characters' as platform
+    {% else %}
+        'okthisis20characters' as platform
+    {% endif %}
+"""
+
+class TestBaseIncrementalNotSchemaChange(BaseIncrementalNotSchemaChange):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {"incremental_not_schema_change.sql": incremental_not_schema_change_sql}
