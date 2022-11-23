a9a5966f00801a882ee4d4e37366ddc6bed5cca9
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-11-23 19:06:52 +0530

    change tests to use string arrays

--- tests/functional/adapter/test_utils.py
+++ tests/functional/adapter/test_utils.py
@@ -763,34 +763,34 @@ class TestDateDiff(BaseDateDiff):
         }
 
 models__array_append_expected_sql = """
-select 1 as id, {{ array_construct(["1","2","3","4"]) }} as array_col union all
-select 2 as id, {{ array_construct(["0", "4"]) }} as array_col
+select 1 as id, {{ array_construct(["1","2","3","4"], "string") }} as array_col union all
+select 2 as id, {{ array_construct(["0", "4"], "string") }} as array_col
 """
 
 
 models__array_append_actual_sql = """
-select 1 as id, {{ array_append(array_construct(["1","2","3"]), "4") }} as array_col union all
-select 2 as id, {{ array_append(array_construct(["0"]), "4") }} as array_col
+select 1 as id, {{ array_append(array_construct(["1","2","3"], "string"), "4") }} as array_col union all
+select 2 as id, {{ array_append(array_construct(["0"], "string"), "4") }} as array_col
 """
 class TestArrayAppend(BaseArrayAppend):
     @pytest.fixture(scope="class")
     def models(self):
         return {
             "actual.sql": models__array_append_actual_sql,
-            "expected.sql": models__array_construct_expected_sql,
+            "expected.sql": models__array_append_expected_sql,
         }
 
 
 models__array_concat_expected_sql = """
-select 1 as id, {{ array_construct(["1","2","3","4","5","6"]) }} as array_col union all
-select 2 as id, {{ array_construct(["2"]) }} as array_col union all
-select 3 as id, {{ array_construct(["3"]) }} as array_col
+select 1 as id, {{ array_construct(["1","2","3","4","5","6"], "string") }} as array_col union all
+select 2 as id, {{ array_construct(["0", "2"], "string") }} as array_col union all
+select 3 as id, {{ array_construct(["3", "0"], "string") }} as array_col
 """
 
 models__array_concat_actual_sql = """
-select 1 as id, {{ array_concat(array_construct(["1","2","3"]), array_construct(["4","5","6"])) }} as array_col union all
-select 2 as id, {{ array_concat(array_construct(["0"]), array_construct(["2"])) }} as array_col union all
-select 3 as id, {{ array_concat(array_construct(["3"]), array_construct(["0"])) }} as array_col
+select 1 as id, {{ array_concat(array_construct(["1","2","3"], "string"), array_construct(["4","5","6"], "string")) }} as array_col union all
+select 2 as id, {{ array_concat(array_construct(["0"], "string"), array_construct(["2"], "string")) }} as array_col union all
+select 3 as id, {{ array_concat(array_construct(["3"], "string"), array_construct(["0"], "string")) }} as array_col
 """
 class TestArrayConcat(BaseArrayConcat):
     @pytest.fixture(scope="class")
