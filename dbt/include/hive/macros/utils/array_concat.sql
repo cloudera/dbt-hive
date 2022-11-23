396ebe66f704ca7b13e4ed59b666c96aa00f8adb
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-11-23 19:06:26 +0530

    fix array functions to support string type

--- dbt/include/hive/macros/utils/array_concat.sql
+++ dbt/include/hive/macros/utils/array_concat.sql
@@ -1,3 +1,3 @@
 {% macro hive__array_concat(array_1, array_2) -%}
-    concat_ws(',', {{ array_1 }}, {{ array_2 }})
+    split(concat_ws(',', {{ array_1 }}, {{ array_2 }}), ',')
 {%- endmacro %}
--- dbt/include/hive/macros/utils/array_construct.sql
+++ dbt/include/hive/macros/utils/array_construct.sql
@@ -1,7 +1,15 @@
 {% macro hive__array_construct(inputs, data_type) -%}
     {% if inputs|length > 0 %}
-    array( {{ inputs|join(' , ') }} )
+       {% if data_type == 'string' %} 
+          array( {{ '\"' + inputs|join('\", \"') + '\"' }} )
+       {% else %}
+          array( {{ inputs|join(', ')  }} )
+       {% endif %}
     {% else %}
-    array()
+       {% if data_type == 'string' %} 
+          array("")
+       {% else %}
+          array()
+       {% endif %}
     {% endif %}
 {%- endmacro %}
