495134ee6ae74e184cdd7657caf67618575956fa
Nitesh Yadav <nitesh@cloudera.com>
2023-07-17 08:54:02 -0700

    DBT-717: bugfix datediff marco to handle the boundary conditions

--- dbt/include/hive/macros/utils/datediff.sql
+++ dbt/include/hive/macros/utils/datediff.sql
@@ -23,29 +23,32 @@
     {%- elif datepart == 'week' -%}
 
         case when {{first_date}} < {{second_date}}
-            then
-                ceil( datediff({{second_date}}, {{first_date}}) / 7 )
-            else
-                floor( datediff({{second_date}}, {{first_date}}) / 7 )
+            then floor( datediff({{second_date}}, {{first_date}}) / 7 )
+            else ceil( datediff({{second_date}}, {{first_date}}) / 7 )
             end
+
+        -- did we cross a week boundary (Sunday)
+        + case
+            when {{first_date}} < {{second_date}} and dayofweek(cast({{second_date}} as timestamp)) < dayofweek(cast({{first_date}} as timestamp)) then 1
+            when {{first_date}} > {{second_date}} and dayofweek(cast({{second_date}} as timestamp)) > dayofweek(cast({{first_date}} as timestamp)) then -1
+            else 0 end
 
     {%- elif datepart == 'month' -%}
 
         case when {{first_date}} < {{second_date}}
-            then
-                ceil(months_between({{second_date}}, {{first_date}}))
-            else
-                floor(months_between({{second_date}}, {{first_date}}))
+            then floor(months_between({{second_date}}, {{first_date}}))
+            else ceil(months_between({{second_date}}, {{first_date}}))
             end
+
+        -- did we cross a month boundary?
+        + case
+            when {{first_date}} < {{second_date}} and dayofmonth(cast({{second_date}} as timestamp)) < dayofmonth(cast({{first_date}} as timestamp)) then 1
+            when {{first_date}} > {{second_date}} and dayofmonth(cast({{second_date}} as timestamp)) > dayofmonth(cast({{first_date}} as timestamp)) then -1
+            else 0 end
 
     {%- elif datepart == 'quarter' -%}
 
-        case when {{first_date}} < {{second_date}}
-            then
-                ceil( months_between({{second_date}}, {{first_date}}) / 3 )
-            else
-                floor( months_between({{second_date}}, {{first_date}}) / 3 )
-            end
+        ((year({{second_date}}) - year({{first_date}})) * 4 + quarter({{second_date}}) - quarter({{first_date}}))
 
     {%- elif datepart == 'year' -%}
 
