22b1b257cbf70f692bfacb77aff2c1e66bfad474
niteshy <nitesh.yadav.cse@gmail.com>
2023-07-17 16:07:25 -0700

    DBT-714: Fixing the TestBoolOr test, remove the usage of keyword key from the query (#109)
    Co-authored-by: Nitesh Yadav <nitesh@cloudera.com>

--- tests/functional/adapter/test_utils.py
+++ tests/functional/adapter/test_utils.py
@@ -190,38 +190,6 @@ class TestAnyValue(BaseAnyValue):
         }
 
 
-models__test_bool_or_sql = """
-with util_data as (
-
-    select * from {{ ref('data_bool_or') }}
-
-),
-
-data_output as (
-
-    select * from {{ ref('data_bool_or_expected') }}
-
-),
-
-calculate as (
-
-    select
-        key,
-        {{ bool_or('val1 = val2') }} as value
-    from util_data
-    group by key
-
-)
-
-select
-    calculate.value as actual,
-    data_output.value as expected
-from calculate
-left join data_output
-on calculate.key = data_output.key
-"""
-
-
 class TestBoolOr(BaseBoolOr):
     @pytest.fixture(scope="class")
     def seeds(self):
