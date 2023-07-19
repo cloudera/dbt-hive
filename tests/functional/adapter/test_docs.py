935915e7a7a0f512c6fd97e25f5a5b23a992c21d
niteshy <nitesh.yadav.cse@gmail.com>
2023-07-19 10:57:34 -0700

    DBT-715: Fixing the docs test ie id should be bigint and removed unwanted functions re-definition (#106)
    Co-authored-by: Nitesh Yadav <nitesh@cloudera.com>

--- tests/functional/adapter/test_docs.py
+++ tests/functional/adapter/test_docs.py
@@ -15,146 +15,13 @@
 import pytest
 
 from datetime import datetime
-from dbt.tests.util import AnyInteger
 from dbt.tests.adapter.basic.test_docs_generate import (
     BaseDocsGenerate,
     BaseDocsGenReferences,
     verify_catalog,
 )
-
+from dbt.tests.adapter.basic.expected_catalog import no_stats, base_expected_catalog
 from dbt.tests.util import run_dbt, rm_file
-
-
-def no_stats():
-    return {
-        "has_stats": {
-            "id": "has_stats",
-            "label": "Has Stats?",
-            "value": False,
-            "description": "Indicates whether there are statistics for this table",
-            "include": False,
-        },
-    }
-
-
-def base_expected_catalog(
-    project,
-    role,
-    id_type,
-    text_type,
-    time_type,
-    view_type,
-    table_type,
-    model_stats,
-    seed_stats=None,
-    case=None,
-    case_columns=False,
-):
-    if case is None:
-
-        def case(x):
-            return x
-
-    col_case = case if case_columns else lambda x: x
-
-    if seed_stats is None:
-        seed_stats = model_stats
-
-    model_database = project.database
-    my_schema_name = case(project.test_schema)
-    alternate_schema = case(project.test_schema + "_test")
-
-    expected_cols = {
-        col_case("id"): {
-            "name": col_case("id"),
-            "index": AnyInteger(),
-            "type": id_type,
-            "comment": None,
-        },
-        col_case("first_name"): {
-            "name": col_case("first_name"),
-            "index": AnyInteger(),
-            "type": text_type,
-            "comment": None,
-        },
-        col_case("email"): {
-            "name": col_case("email"),
-            "index": AnyInteger(),
-            "type": text_type,
-            "comment": None,
-        },
-        col_case("ip_address"): {
-            "name": col_case("ip_address"),
-            "index": AnyInteger(),
-            "type": text_type,
-            "comment": None,
-        },
-        col_case("updated_at"): {
-            "name": col_case("updated_at"),
-            "index": AnyInteger(),
-            "type": time_type,
-            "comment": None,
-        },
-    }
-
-    return {
-        "nodes": {
-            "model.test.model": {
-                "unique_id": "model.test.model",
-                "metadata": {
-                    "schema": my_schema_name,
-                    "database": model_database,
-                    "name": case("model"),
-                    "type": view_type,
-                    "comment": None,
-                    "owner": role,
-                },
-                "stats": model_stats,
-                "columns": expected_cols,
-            },
-            "model.test.second_model": {
-                "unique_id": "model.test.second_model",
-                "metadata": {
-                    "schema": alternate_schema,
-                    "database": project.database,
-                    "name": case("second_model"),
-                    "type": view_type,
-                    "comment": None,
-                    "owner": role,
-                },
-                "stats": model_stats,
-                "columns": expected_cols,
-            },
-            "seed.test.seed": {
-                "unique_id": "seed.test.seed",
-                "metadata": {
-                    "schema": my_schema_name,
-                    "database": project.database,
-                    "name": case("seed"),
-                    "type": table_type,
-                    "comment": None,
-                    "owner": role,
-                },
-                "stats": seed_stats,
-                "columns": expected_cols,
-            },
-        },
-        "sources": {
-            "source.test.my_source.my_table": {
-                "unique_id": "source.test.my_source.my_table",
-                "metadata": {
-                    "schema": my_schema_name,
-                    "database": project.database,
-                    "name": case("seed"),
-                    "type": table_type,
-                    "comment": None,
-                    "owner": role,
-                },
-                "stats": seed_stats,
-                "columns": expected_cols,
-            },
-        },
-    }
 
 
 class TestBaseDocsHive(BaseDocsGenerate):
@@ -163,7 +30,7 @@ class TestBaseDocsHive(BaseDocsGenerate):
         return base_expected_catalog(
             project,
             role=profile_user,
-            id_type="int",
+            id_type="bigint",
             text_type="string",
             time_type="timestamp",
             view_type="view",
