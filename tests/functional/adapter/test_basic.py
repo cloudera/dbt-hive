23a5760f353362117ecddc4b148848825f03f049
niteshy <nitesh.yadav.cse@gmail.com>
2023-08-07 07:59:39 -0700

    DBT-731: Fixing the incremental overwrite strategy insert statements (#120)
    Co-authored-by: Nitesh Yadav <nitesh@cloudera.com>

--- tests/functional/adapter/test_basic.py
+++ tests/functional/adapter/test_basic.py
@@ -168,6 +168,20 @@ select
     {% endif %}
 """
 
+insertoverwrite_sql = """
+{{ config(materialized="incremental", incremental_strategy="insert_overwrite", partition_by="id_partition") }}
+select *, id as id_partition from {{ source('raw', 'seed') }}
+    {% if is_incremental() %}
+        where id > (select max(id) from {{ this }})
+    {% endif %}
+""".strip()
+
+
+class TestInsertOverwriteHive(BaseIncremental):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {"incremental.sql": insertoverwrite_sql, "schema.yml": schema_base_yml}
+
 
 class TestBaseIncrementalNotSchemaChange(BaseIncrementalNotSchemaChange):
     @pytest.fixture(scope="class")
