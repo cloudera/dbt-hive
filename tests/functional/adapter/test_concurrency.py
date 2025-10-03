fc32a2f47c1b91d752887df4ef4bb521a0f61675
Niru <niru@cloudera.com>
2025-10-03 11:53:43 +0530

    DBT-822 Fix-1.10.0-hive Support dbt-core 1.10.0 for dbt-hive (#165)

--- tests/functional/adapter/test_concurrency.py
+++ tests/functional/adapter/test_concurrency.py
@@ -1,23 +1,47 @@
+from collections import Counter
+
 import pytest
-from dbt.tests.util import run_dbt, check_relations_equal, rm_file, write_file
+
+from dbt.artifacts.schemas.results import RunStatus
+from dbt.tests.util import (
+    check_relations_equal,
+    check_table_does_not_exist,
+    rm_file,
+    run_dbt,
+    write_file,
+)
+
 from dbt.tests.adapter.concurrency.test_concurrency import BaseConcurrency, seeds__update_csv
 
 
 class TestConcurrencyHive(BaseConcurrency):
-    def test_concurrency_hive(self, project):
+    def test_concurrency(self, project):
         run_dbt(["seed", "--select", "seed"])
         results = run_dbt(["run"], expect_pass=False)
         assert len(results) == 7
-        check_relations_equal(project.adapter, ["SEED", "VIEW_MODEL"])
-        check_relations_equal(project.adapter, ["SEED", "DEP"])
-        check_relations_equal(project.adapter, ["SEED", "TABLE_A"])
-        check_relations_equal(project.adapter, ["SEED", "TABLE_B"])
+        check_relations_equal(project.adapter, ["seed", "view_model"])
+        check_relations_equal(project.adapter, ["seed", "dep"])
+        check_relations_equal(project.adapter, ["seed", "table_a"])
+        check_relations_equal(project.adapter, ["seed", "table_b"])
+        check_table_does_not_exist(project.adapter, "`invalid`")
+        check_table_does_not_exist(project.adapter, "`skip`")
 
         rm_file(project.project_root, "seeds", "seed.csv")
-        write_file(seeds__update_csv, project.project_root + "/seeds", "seed.csv")
+        write_file(seeds__update_csv, project.project_root, "seeds", "seed.csv")
+
         results = run_dbt(["run"], expect_pass=False)
-        assert len(results) == 7
-        check_relations_equal(project.adapter, ["SEED", "VIEW_MODEL"])
-        check_relations_equal(project.adapter, ["SEED", "DEP"])
-        check_relations_equal(project.adapter, ["SEED", "TABLE_A"])
-        check_relations_equal(project.adapter, ["SEED", "TABLE_B"])
+
+        check_relations_equal(project.adapter, ["seed", "view_model"])
+        check_relations_equal(project.adapter, ["seed", "dep"])
+        check_relations_equal(project.adapter, ["seed", "table_a"])
+        check_relations_equal(project.adapter, ["seed", "table_b"])
+        check_table_does_not_exist(project.adapter, "`invalid`")
+        check_table_does_not_exist(project.adapter, "`skip`")
+
+        result_statuses = Counter([result.status for result in results])
+        expected_statuses = {
+            RunStatus.Success: 5,
+            RunStatus.Error: 1,
+            RunStatus.Skipped: 1,
+        }
+        assert result_statuses == expected_statuses
